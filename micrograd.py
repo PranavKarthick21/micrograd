48f50190ea17265e10b38a699beb2d2bfd58b139
Pranav Karthick S <pranavkarthick.s8@gmail.com>
2026-05-12 12:30:41 +0530

    First commit

---
+++ micrograd.py
@@ -0,0 +1,95 @@
+import math
+import random
+class Value:
+    def __init__(self,data,_children=()):
+        self.data=data
+        self.grad=0.0
+        self._prev=set(_children)
+        self._backward=lambda:None
+        
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    def __add__(self,other):
+        other=other if isinstance(other,Value) else Value(other)
+        out=Value(self.data+other.data,(self,other))
+        def _backward():
+            self.grad+=1.0*out.grad
+            other.grad+=1.0*out.grad
+        out._backward=_backward
+        return out
+    def __mul__(self,other):
+        other=other if isinstance(other,Value) else Value(other)
+        out=Value(self.data*other.data,(self,other))
+        def _backward():
+            self.grad+=other.data*out.grad
+            other.grad+=self.data*out.grad
+        out._backward=_backward
+        return out
+    def __pow__(self,other):
+        assert isinstance(other,(int,float)),"only supporting int/float powers"
+        out=Value(self.data**other,(self,))
+        def _backward():
+            self.grad+=other*(self.data**(other-1))*out.grad
+        out._backward=_backward
+        return out
+    def __rmul__(self,other):
+        return self*other
+    def __truediv__(self,other):
+        return self*other**-1
+    def __sub__(self,other):
+        return self+(-other)
+    def __radd__(self,other):
+        return self+other
+    def __neg__(self):
+        return self*-1;
+    def tanh(self):
+        x=self.data
+        t=(math.exp(2*x)-1)/(math.exp(2*x)+1)
+        out=Value(t,(self,))
+        def _backward():
+            self.grad+=(1-t**2)*out.grad
+        out._backward=_backward
+        return out
+    def backward(self):
+        topo=[]
+        visited=set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad=1.0
+        for node in reversed(topo):
+            node._backward()
+
+class Neuron:
+    def __init__(self,nin):
+        self.w=[Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b=Value(random.uniform(-1,1))
+
+    def __call__(self,x):
+        act=sum((wi*xi for wi,xi in zip(self.w,x)),self.b)
+        out=act.tanh()
+        return out
+    def parameters(self):
+        return self.w+[self.b]
+class Layer:
+    def __init__(self,nin,nout):
+        self.neurons=[Neuron(nin) for _ in range(nout)]
+    def __call__(self,x):
+        outs=[n(x) for n in self.neurons]
+        return outs[0] if len(outs)==1 else outs
+    def parameters(self):
+        return [p for neurons in self.neurons for p in neurons.parameters()]
+class MLP:
+    def __init__(self,nin,nouts):
+        sz=[nin]+nouts
+        self.layers=[Layer(sz[i],sz[i+1]) for i in range(len(nouts))]
+    def __call__(self,x):
+        for layer in self.layers:
+            x=layer(x)
+        return x
+    def parameters(self):
+        return[p for layer in self.layers for p in layer.parameters()]
